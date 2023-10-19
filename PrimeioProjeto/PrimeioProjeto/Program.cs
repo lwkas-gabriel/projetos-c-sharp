85036d9c85f436b881ee7992173f2fb9d313fff5
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-19 08:51:32 -0300

    commit inical

---
+++ PrimeioProjeto/PrimeioProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int teste = 0;
+            int teste1 = 2;
+            Console.WriteLine("" + teste + teste1);
+        }
+    }
+}
