0d5dedbdd22284d06b75e8557eaebe4250ba0285
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-19 13:05:11 -0300

    exercicio classes, construtor e métodos.

---
+++ Course/Course/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Produto
+    {
+        private string Nome;
+        private double Preco;
+        private int Quantidade;
+
+        public Produto(string nome, double preco, int qtd)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = qtd;
+        }
+
+        public void ImprimirDadosProduto()
+        {
+            Console.WriteLine($"{this.Nome} - Quantidade em Estoque: {this.Quantidade} - Preço: R${this.Preco}");
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return this.Quantidade;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            if (qtd < 0)
+            {
+                Console.WriteLine("Entrada inválida!");
+            }
+            else
+            {
+                this.Quantidade += qtd;
+            }
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            if (qtd > this.Quantidade)
+            {
+                Console.WriteLine("A quantidade informada he maior que a quantidade em estoque!");
+            }
+            else
+            {
+                this.Quantidade -= qtd;
+            }
+        }
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,31 +7,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            /*Triangulo x = new Triangulo();
-            Triangulo y = new Triangulo();
-
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
-
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com as medidas do triângulo Y: ");
-
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-
-
-            Console.WriteLine("Area do triangulo X: " + areaX);
-            Console.WriteLine("Area do triangulo Y: " + areaY);
-
+            /*
             Pessoa primeiraPessoa = new Pessoa();
             Pessoa segundaPessoa = new Pessoa();
 
@@ -56,7 +32,7 @@ namespace Course
             {
                 Console.WriteLine($"Pessoa mais velha: {segundaPessoa.Nome}");
             }
-            Console.WriteLine();*/
+            Console.Read();
 
             Triangulo x = new Triangulo();
             Triangulo y = new Triangulo();
@@ -79,7 +55,15 @@ namespace Course
 
             Console.WriteLine("Area do triangulo X: " + areaX);
             Console.WriteLine("Area do triangulo Y: " + areaY);
+            Console.Read();*/
+
+            Produto novoProduto = new Produto("Mandioca", 1.45, 50);
+            novoProduto.ImprimirDadosProduto();
+            novoProduto.AdicionarProdutos(10);
+            novoProduto.RemoverProdutos(15);
+            novoProduto.ImprimirDadosProduto();
             Console.Read();
+
         }
     }
 }
