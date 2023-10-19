902302111487b38f278ff0773f665b6d30956243
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-19 12:39:40 -0300

    aula sobre funções

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,26 +1,85 @@
-﻿using Course;
+﻿using System;
 using System.Globalization;
 
-Triangulo x = new Triangulo();
-Triangulo y = new Triangulo();
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
 
-Console.WriteLine("Entre com as medidas do triângulo X: ");
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
 
-x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
 
-y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-double p = (x.A + x.B + x.C) / 2.0;
-double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-p = (y.A + y.B + y.C) / 2.0;
-double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
-Console.WriteLine("Area do triangulo X: " + areaX);
-Console.WriteLine("Area do triangulo Y: " + areaY);
+
+            Console.WriteLine("Area do triangulo X: " + areaX);
+            Console.WriteLine("Area do triangulo Y: " + areaY);
+
+            Pessoa primeiraPessoa = new Pessoa();
+            Pessoa segundaPessoa = new Pessoa();
+
+            Console.WriteLine("Inserir dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            primeiraPessoa.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            primeiraPessoa.Idade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Inserir dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            segundaPessoa.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            segundaPessoa.Idade = int.Parse(Console.ReadLine());
+
+            if (primeiraPessoa.Idade > segundaPessoa.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {primeiraPessoa.Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {segundaPessoa.Nome}");
+            }
+            Console.WriteLine();*/
+
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+
+            Console.WriteLine("Area do triangulo X: " + areaX);
+            Console.WriteLine("Area do triangulo Y: " + areaY);
+            Console.Read();
+        }
+    }
+}
--- Course/Course/Triangulo.cs
+++ Course/Course/Triangulo.cs
@@ -11,5 +11,13 @@ namespace Course
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+
     }
 }
