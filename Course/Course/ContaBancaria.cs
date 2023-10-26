0b3baa53f484ce0a91d1149798c3ee9183f8aca7
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-26 20:46:45 -0300

    exercicio contaBancaria

---
+++ Course/Course/ContaBancaria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            this.Titular = titular;
+            this.NumeroConta = numero;
+            //se não informado, o saldo eh inicializado com 0.0
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo): this(numero, titular)
+        {
+            this.Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,64 +7,31 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            /*
-            Pessoa primeiraPessoa = new Pessoa();
-            Pessoa segundaPessoa = new Pessoa();
+            ContaBancaria contaNova;
 
-            Console.WriteLine("Inserir dados da primeira pessoa: ");
-            Console.Write("Nome: ");
-            primeiraPessoa.Nome = Console.ReadLine();
-            Console.Write("Idade: ");
-            primeiraPessoa.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
 
+            Console.Write("Entre com o titula da conta: ");
+            string titular = Console.ReadLine();
 
-            Console.WriteLine("Inserir dados da segunda pessoa: ");
-            Console.Write("Nome: ");
-            segundaPessoa.Nome = Console.ReadLine();
-            Console.Write("Idade: ");
-            segundaPessoa.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Haverá deposito inicial? (s/n)");
+            char resp = char.Parse(Console.ReadLine());
 
-            if (primeiraPessoa.Idade > segundaPessoa.Idade)
+            if (resp == 's' || resp == 'S')
             {
-                Console.WriteLine($"Pessoa mais velha: {primeiraPessoa.Nome}");
+                Console.WriteLine("Digite o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                contaNova = new ContaBancaria(numero, titular, depositoInicial);
             }
             else
             {
-                Console.WriteLine($"Pessoa mais velha: {segundaPessoa.Nome}");
+                contaNova = new ContaBancaria(numero, titular);
             }
-            Console.Read();
 
-            Triangulo x = new Triangulo();
-            Triangulo y = new Triangulo();
-
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
-
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com as medidas do triângulo Y: ");
-
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double areaX = x.Area();
-            double areaY = y.Area();
-
-
-            Console.WriteLine("Area do triangulo X: " + areaX);
-            Console.WriteLine("Area do triangulo Y: " + areaY);
-            Console.Read();
-
-            Produto novoProduto = new Produto("Mandioca", 1.45, 50);
-            novoProduto.ImprimirDadosProduto();
-            novoProduto.AdicionarProdutos(10);
-            novoProduto.RemoverProdutos(15);
-            novoProduto.ImprimirDadosProduto();*/
-
-            
-
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaNova);
 
             Console.Read();
 
